d7ff471b76688bcba47e4172f0904e90b438746d
Agustin G. <agustin.gomez.mansilla@gmail.com>
2025-01-29 15:11:07 -0300

    add kitsune sculpture

--- src/sculptures.ts
+++ src/sculptures.ts
@@ -5,6 +5,7 @@ import onirica from './assets/onirica.jpeg';
 import paternidad from './assets/paternidad.jpeg';
 import pesebre from './assets/pesebre.jpeg';
 import equilibrio from './assets/enEquilibrio.jpeg'
+import kitsune from './assets/kitsune.jpeg';
 
 
 interface Sculpture {
@@ -14,41 +15,46 @@ interface Sculpture {
 }
 
 const sculptures: Sculpture[] = [
-  {
-      key: "equilibrio",
-      image: equilibrio,
-      size: ""
-  },
-  {
-    key: "madreDeDos",
-    image: madreDeDos,
-    size: "15x25x37 cm"
-  },
-  {
-    key: "dormision",
-    image: dormision,
-    size: ""
-  },
-  {
-    key: "paternidad",
-    image: paternidad,
-    size: ""
-  },
-  {
-    key: "onirica",
-    image: onirica,
-    size: ""
-  },
-  {
-    key: "pesebre",
-    image: pesebre,
-    size: ""
-  },
-  {
-    key: "sirena",
-    image: sirena,
-    size: ""
-  },
+    {
+        key: "equilibrio",
+        image: equilibrio,
+        size: ""
+    },
+    {
+        key: "kitsune",
+        image: kitsune,
+        size: ""
+    },
+    {
+        key: "madreDeDos",
+        image: madreDeDos,
+        size: "15x25x37 cm"
+    },
+    {
+        key: "dormision",
+        image: dormision,
+        size: ""
+    },
+    {
+        key: "paternidad",
+        image: paternidad,
+        size: ""
+    },
+    {
+        key: "onirica",
+        image: onirica,
+        size: ""
+    },
+    {
+        key: "pesebre",
+        image: pesebre,
+        size: ""
+    },
+    {
+        key: "sirena",
+        image: sirena,
+        size: ""
+    },
 ];
 
 export default sculptures;
